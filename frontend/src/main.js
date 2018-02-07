b37d91f8db291821a2c41152b689e4c7a775ac09
Ronald Huggler <ronald@huggler.com.br>
2018-02-07 01:57:48 -0200

    Fix import App

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,9 @@
 /* eslint-disable no-new */
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import Upload from './App.vue' /* 2/5 */
 
 Vue.config.productionTip = false
-
-new Vue({                    /* 2/5 */
-  el: '#upload',             /* 2/5 */
-  render: h => h(Upload)     /* 2/5 ... what does this line do? */
-})                           /* 2/5 */
 
 new Vue({
   el: '#app',
